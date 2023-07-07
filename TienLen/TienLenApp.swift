6a0e699a501ae5d1d9f4e9731eb3e07ffc98a0f4
Dylan Dao <dylanpdao@gmail.com>
2023-07-07 09:44:15 -0700

    Initial Commit

---
+++ TienLen/TienLenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TienLenApp.swift
+//  TienLen
+//
+//  Created by Dylan Dao on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TienLenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
